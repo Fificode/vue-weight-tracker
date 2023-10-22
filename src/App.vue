cdbe9467194694e315257e7b57a34844314fb057
Fificode <lamidifirdaws@gmail.com>
2023-10-22 23:43:55 +0100

    deleted InputSection and tried displaying weight history

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,82 @@
     <header class='header'>
       <h1>Weight Tracker App</h1>
     </header>
-    <InputSection />
-    <RecentWeightsSection />
-    <ChartSection />
+    <main>
+      <div>
+    <div class="current">
+      <h3>{{ submittedInput }}</h3>
+      <h4>Current weight (kg)</h4>
+      </div>
+    <form @submit.prevent="addWeight" class="form">
+        <input type="number" step="0.1" v-model="weightInput" class="input-number"/><br/>
+        <input type="submit" value="Add weight" class="input-submit"/>
+        </form>
+</div>
+    </main>
+    
+    <h2>Last 7 days</h2>
+    <ChartSection/>
+    <h2>Weight History</h2>
+    <RecentWeightsSection :recentWeights="weights.value"/> 
   </div>
 </template>
 
 <script>
-import InputSection from './components/InputSection.vue';
-import ChartSection from './components/ChartSection.vue';
-import RecentWeightsSection from './components/RecentWeightsSection.vue'
 
+import RecentWeightsSection from './components/RecentWeightsSection.vue';
+import ChartSection from './components/ChartSection.vue';
+
+
+import { ref, shallowRef } from 'vue'
 export default {
   name: 'App',
   components: {
-    InputSection,
-    ChartSection,
+    
     RecentWeightsSection,
-  }
+    ChartSection
+   
+  },
+  setup(){
+        
+        //Initialized an empty array so the inputed weights and date will be pushed into 
+        const weights = shallowRef([])
+        //Initialized the inputed weight to 0.0
+        const weightInput = ref(0.0)
+        const submittedInput = ref(0.0)
+        let weightsValue = shallowRef([])
+
+const addWeight = () => {
+    //Show weight on screen
+submittedInput.value = weightInput.value;
+//  weightsValue = weights.value
+//Add weight and date to an array
+ weights.value.push({
+    weight: submittedInput.value, 
+    date: new Date().getTime()
+   })
+  //  console.log(weights.value)
+  //  if(weights.value.length > 0 ){
+  //  weights.value.sort((a,b) => b.date - a.date)
+  // }
+        }
+
+return {
+            weights,
+            weightInput,
+            weightsValue,
+            submittedInput,
+            addWeight,
+           
+         
+            
+        }
+    },
+  
+ 
 }
 </script>
 
-<style>
+<style >
 body {
   margin: 0;
   background: #fff;
@@ -40,7 +94,45 @@ body {
   color: #2c3e50;
   margin-top: 60px;
 }
-.header h1{
+ h1{
   text-align: center;
+}
+h2{
+ text-align: center;
+  padding-top: 50px;
+}
+.form{
+   display: flex;
+   flex-direction: column;
+   justify-content: center; 
+   align-items: center;
+}
+input{
+   
+    
+    border: 1px solid #ddd;
+    
+}
+.input-number{
+    border-radius: 2px;
+    width: 70%;
+    height: 30px;
+    padding: 5px;
+    outline-color:blueviolet ;
+}
+.input-submit{
+    border-radius: 3px;
+    height: 35px;
+    margin-top: 10px;
+    width: 50%;
+    cursor: pointer;
+    background-color: blueviolet;
+    outline-color:#fff ;
+    color: #fff;
+
+}
+.current{
+    margin-top: 60px;
+    margin-bottom: 30px;
 }
 </style>
--- src/components/RecentWeightsSection.vue
+++ src/components/RecentWeightsSection.vue
@@ -1,11 +1,31 @@
 <template>
     <div>
-        <h1>7 days</h1>
+        <div class="weight-history">
+              <!-- <ul>
+                <li v-for="weight in recentWeights" v-bind:key="date in recentWeights">
+                  <span>{{ weight.weight }}kg</span>
+                  <small>{{ new Date(weight.date).toLocaleDateString() }}</small>
+                  </li>
+        
+                </ul> -->
+              </div>
+        <!-- <p>{{this.recentWeight}}</p> -->
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Recent-Weight'
+    name: 'Recent-Weight',
+    props: {
+    recentWeights: Array, 
+   
+  }, 
+    mounted(){
+// console.log(this.recentWeights, 'mounted')
+    },
+    updated(){
+        // console.log(this.recentWeights, 'updated')
+    }
+  
 }
 </script>
